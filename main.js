a9ed6e0aa71d3741767057ef927c470b2cea2507
Zoheb <zoheb@example.com>
2026-08-25 00:46:53 +0530

    Restore state for Pixels and Custom Sequence from MQTT retained message

--- main.js
+++ main.js
@@ -359,6 +359,28 @@ function toggleTvPower() {
                         document.getElementById('tvSpeed').value = tvSpeed;
                         document.getElementById('tvSpeedVal').innerText = tvSpeed + '%';
                     }
+                    if (data.pixels !== undefined) {
+                        tvPixels = data.pixels;
+                        document.getElementById('tvPixels').value = tvPixels;
+                    }
+                    if (data.c_seg !== undefined) {
+                        cSeg = data.c_seg;
+                        document.getElementById('cSeg').value = cSeg;
+                        document.getElementById('cSegVal').innerText = cSeg;
+                    }
+                    if (data.c_del !== undefined) {
+                        cDel = data.c_del;
+                        document.getElementById('cDel').value = cDel;
+                        document.getElementById('cDelVal').innerText = (cDel / 1000).toFixed(1) + 's';
+                    }
+                    if (data.c_acc !== undefined) {
+                        cAcc = data.c_acc;
+                        document.getElementById('cAcc').checked = cAcc;
+                    }
+                    if (data.c_seq !== undefined) {
+                        cSeqStr = data.c_seq.join('-');
+                        document.getElementById('cSeqStr').value = cSeqStr;
+                    }
                                       if (data.effect !== undefined) {
                       tvEffect = data.effect;
                       document.querySelectorAll('.tv-effect-btn').forEach(btn => {
