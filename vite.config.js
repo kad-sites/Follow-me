001d2c3bc74e4dd40110eb4043db6e9175d878e8
Zoheb <zoheb@example.com>
2026-08-24 17:07:57 +0530

    Destroy PWA service worker to fix caching

--- vite.config.js
+++ vite.config.js
@@ -5,30 +5,7 @@ export default defineConfig({
   base: './',
   plugins: [
     VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: 'Follow Me LED',
-        short_name: 'LED',
-        description: 'Smart Corridor Light Controller',
-        theme_color: '#0d0d0d',
-        background_color: '#0d0d0d',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
+      selfDestroying: true
     })
   ]
 })
