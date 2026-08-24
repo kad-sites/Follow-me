033ac3f9aac04f623b510c9b9156c2b22725bece
Zoheb <zoheb@example.com>
2026-08-25 00:23:30 +0530

    Disable PWA to completely eliminate caching

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: './',
-  plugins: [
-    VitePWA({
-      selfDestroying: true
-    })
-  ]
+  base: './'
 })
